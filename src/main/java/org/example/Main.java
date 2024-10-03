77ccaa9d3d47c494db382bb4859def3ee823b650
CifronPro <ars132465@gmail.com>
2024-10-03 23:01:45 +0700

    init commit include dependencies

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import com.google.gson.Gson;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+    }
+}
